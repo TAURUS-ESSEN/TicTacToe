6d6f8aaedf061ab2d582e9c5df49196b3213a709
TAURUS-ESSEN <reitarovea@gmail.com>
2025-02-25 20:20:06 +0100

    DisplayController module

--- script.js
+++ script.js
@@ -1,14 +1,9 @@
 'use strict';
 const Gameboard = {
     gameboard:  Array(9).fill(""),
-   // moveCounter: 0,
-    //currentPlayer: "X",
     gameboardReset: function() {
         gameboard = Array(9).fill("")
     },
-
-    
-
 }
 
 function createPlayer(name,marker) {
@@ -21,12 +16,10 @@ function createPlayer(name,marker) {
 const gameControl = (function () {
     let moveCounter = 0;
     let currentPlayer = "X";
+    let isRobot = false;
     return {
         counterIncrement: function() {
             moveCounter++;
-        },
-        counterDecrement1: function() {
-            moveCounter--;
         },
         counterShow: function() {
             return moveCounter;
@@ -39,177 +32,187 @@ const gameControl = (function () {
             moveCounter = 0;
             currentPlayer = "X";
         },
+        updateRobotStatus: function() {
+            isRobot = checkbox.checked;
+            console.log(isRobot);
+            return isRobot;
+        },
     };
 })();
-  
-const player1 = createPlayer("player1", "X");
-const player2 = createPlayer("player2", "O");
 
-//сокращения
+const displayController = ( function() {
+
+})()
+  
+const player1 = createPlayer("Player1", "X");
+const player2 = createPlayer("Player2", "O");
 
 let gameboard = Gameboard.gameboard; 
 let moveCounter = gameControl.counterShow;
-//let marker =  Gameboard.currentPlayer;
- 
+let currentMarker = gameControl.currentPlayerChange; 
 
 function makeTurn(currentButton) {
     if (gameboard[currentButton.value] === "") {
         document.querySelectorAll("input").forEach(function(input) {
-            input.disabled = true; });
-        currentButton.textContent = gameControl.currentPlayerChange(); 
-        if (gameControl.currentPlayerChange() == "X") {
-            currentButton.style.color = "blue";
-        }
-        if (gameControl.currentPlayerChange() == "O") {
-            currentButton.style.color = "red";
-        }
-      //  console.log("Доска до нажатия ", gameboard);
-        console.log("счетчик до =", moveCounter());
-        gameboard[currentButton.value] = gameControl.currentPlayerChange();
-        console.log("доска после нажатия = ", gameboard);
-       
+            input.disabled = true; }); // When the game starts, all inputs are deactivated
+
+        currentButton.textContent = currentMarker(); 
+        currentButton.style.color = (moveCounter() %2 == 0) ? "blue" : "red";
         
-        currentButton.disabled = "true"
-        let result1 =checkResult1(gameboard, moveCounter());
-            if (result1 == "win") {
-                 winMessage(moveCounter(), gameControl.currentPlayerChange()); 
+        gameboard[currentButton.value] = currentMarker(); // array[index] = X or O
+        console.log("current gameboard = ", gameboard);
+        currentButton.disabled = "true"; 
+        
+        let result = checkResult(gameboard); // check win oder draw
+            if (result == "win") {
+                 winMessage(currentMarker()); 
             }
-            if (result1 == "draw") {
+            if (result == "draw") {
                 drawMessage();     
             } 
-        gameControl.counterIncrement();
-        console.log("счетчик ПОСЛЕ =", moveCounter());
+
+        gameControl.counterIncrement(); //  counter +1
+      
+        if (gameControl.updateRobotStatus() == true) { // play with robot?
+            if (result == "win") {
+                winMessage(currentMarker); 
+                return;
+            }
+
+            if (!gameboard.includes("")) {
+                drawMessage();
+                return;
+            }
+            setTimeout(() => {
+                robotTurn();
+            }, 500);  // Delay in displaying the computer's response for smoother gameplay.
+        }
     }
 }
 
-function checkResult1(gameboard, moveCounter) {
-    console.log("--------------------------");
-    console.log("проверка хода #" +moveCounter);
+function robotTurn() {
+    let currentMarker = gameControl.currentPlayerChange();
+    
+    let randomIndex;
+    do {
+        randomIndex = Math.floor(Math.random() * 9); //random index number from 0 ... 8
+    } while (gameboard[randomIndex] !== "");  
 
+    gameboard[randomIndex] = currentMarker;     //array[random index] = O
+
+    let currentButton1 = document.querySelector(`button[value="${randomIndex}"]`); // select buttom with value = randomIndex
+    currentButton1.disabled = "true"; // deactivate the button to prevent repeated clicks.
+    currentButton1.style.color = (moveCounter() %2 == 0) ? "blue" : "red";
+    currentButton1.textContent = currentMarker; 
+    console.log("gameboard #", +moveCounter, gameboard);  
+    gameControl.counterIncrement();// counter +1
+    let result =checkResult(gameboard); // // check win oder draw
+    if (result == "win") {
+         winMessage(moveCounter(), currentMarker); 
+    }
+    if (result == "draw") {
+         drawMessage();     
+    } 
+}
+
+function checkResult(gameboard) {
 // check lines    
-for (let i = 0; i < 9; i += 3) {
-    let threeInRow = gameboard.slice(i, i + 3);
-    console.log(`проверяем по строкам`);
-    console.log(threeInRow);
-    if (threeInRow.every(el => el !== "")) {
-        if (new Set(threeInRow).size === 1) {
+    for (let i = 0; i < 9; i += 3) {
+        let threeInRow = gameboard.slice(i, i + 3);
+        if (threeInRow.every(el => el !== "")) {
+            if (new Set(threeInRow).size === 1) {
+                return "win";
+            }
+        }
+    }
+
+// check columns
+    for (let i = 0; i < 3; i += 1) {
+        let threeInColumn = [gameboard[i],gameboard[i+3],gameboard[i+6]];
+        if (threeInColumn.every(el => el !== "")) {
+            if (new Set(threeInColumn).size === 1 ) {
+                return "win";
+            }
+        }
+    }  
+// check diagonals
+    let leftToRightDiagonal= [gameboard[0],gameboard[4],gameboard[8]];
+    if (leftToRightDiagonal.every(el => el !== "")) {
+        if (new Set(leftToRightDiagonal).size === 1) {
             return "win";
         }
     }
-}
 
-// check columns
-for (let i = 0; i < 3; i += 1) {
-    let threeInColumn = [gameboard[i],gameboard[i+3],gameboard[i+6]];
-    console.log(`проверяем по столбцам`);
-    console.log(threeInColumn);
-    if (threeInColumn.every(el => el !== "")) {
-    if (new Set(threeInColumn).size === 1 ) {
-       return "win";
+    let rightToLeftDiagonal= [gameboard[2],gameboard[4],gameboard[6]];
+    if (rightToLeftDiagonal.every(el => el !== "")) {
+        if (new Set(rightToLeftDiagonal).size === 1) {
+            return "win";
+        }
     }
-  }
-}  
-// check diagonals
-  let leftToRightDiagonal= [gameboard[0],gameboard[4],gameboard[8]];
-  if (leftToRightDiagonal.every(el => el !== "")) {
-  if (new Set(leftToRightDiagonal).size === 1) {
-        return "win";
-    }
- }
-
-let rightToLeftDiagonal= [gameboard[2],gameboard[4],gameboard[6]];
-if (rightToLeftDiagonal.every(el => el !== "")) {
-if (new Set(rightToLeftDiagonal).size === 1) {
-        return "win";
-}
-}
 
 // check draw
-if (!gameboard.includes("")) {
-    console.log("теперь доска"+gameboard);
-    return `draw`;
-}
+    if (!gameboard.includes("")) {
+        console.log("gameboard", gameboard);
+        return `draw`;
+    }
 } 
 
 function createNewButton() {
-    const resetBtn = document.createElement("button");
-    resetBtn.textContent = "New Game";
-    resetBtn.classList = "resetBtn";
-    document.getElementById("endGame").appendChild(resetBtn);
-    resetBtn.addEventListener("click", function() {
-        reset() })
+    const newGameBtn = document.createElement("button");
+    newGameBtn.textContent = "New Game";
+    newGameBtn.classList = "newGameBtn";
+    document.getElementById("endGame").appendChild(newGameBtn);
+    newGameBtn.addEventListener("click", function() {
+        reset() 
+    })
 }
 
 function reset() {
     Gameboard.gameboardReset();
-    console.log("сброс доски = ", gameboard);
+    console.log("board reset = ", gameboard);
     document.getElementById("endGame").remove();
     document.querySelectorAll(".gameButton").forEach(function(button) {
         button.disabled = false;
         button.textContent = "";
         gameControl.gameReset();
-      });
-      document.querySelectorAll("input").forEach(function(input) {
-        input.disabled = false; });
-     
-
+    });
+    document.querySelectorAll("input").forEach(function(input) {
+        input.disabled = false;
+    });     
 }
 
-function winMessage(moveCounter, marker) {
-    //marker == player1.marker ? console.log("победил " +player1.name) :  console.log("победил " +player2.name);
-    let winner = marker == player1.marker ? player1.name : player2.name
-    //console.log("тут будет текст про победу. Игра закончена на #"+moveCounter + " Победа за " +marker);
-    document.getElementById("footer").innerHTML = `<div id="endGame"><p>Player ${winner}  ${marker}  wins</p></div>`;
+function winMessage(currentMarker) {
+   let winner = currentMarker == player1.marker ? player1.name : player2.name
+    document.getElementById("footer").innerHTML = `<div id="endGame"><p> ${winner} wins</p></div>`;
     document.querySelectorAll(".gameButton").forEach(function(button) {
         button.disabled = true;
       });
-     
-    createNewButton()
+    createNewButton();
 }
 
 function drawMessage() {
-    console.log("Ничья");
     document.getElementById("footer").innerHTML = `<div id="endGame"><p>Draw</p></div>`;
-    createNewButton()
+    createNewButton();
 }
 
-
-const btn0 = document.getElementById("button0");
-btn0.addEventListener("click", function() {
-   makeTurn(btn0) })
-const btn1 = document.getElementById("button1");
-btn1.addEventListener("click", function() {
-    makeTurn(btn1) })
-const btn2 = document.getElementById("button2")
-btn2.addEventListener("click", function() {
-    makeTurn(btn2) })
-const btn3 = document.getElementById("button3");
-btn3.addEventListener("click", function() {
-    makeTurn(btn3) })
-const btn4 = document.getElementById("button4");
-btn4.addEventListener("click", function() {
-    makeTurn(btn4) })
-const btn5 = document.getElementById("button5");
-btn5.addEventListener("click", function() {
-    makeTurn(btn5) })
-const btn6 = document.getElementById("button6");
-btn6.addEventListener("click", function() {
-    makeTurn(btn6) })
-const btn7 = document.getElementById("button7");
-btn7.addEventListener("click", function() {
-    makeTurn(btn7) })
-const btn8 = document.getElementById("button8");
-btn8.addEventListener("click", function() {
-    makeTurn(btn8) })
+const buttons = document.querySelectorAll(".gameButton");
+buttons.forEach(button => {
+    button.addEventListener('click', (event) => {
+        makeTurn(event.target);   
+    });
+});
 
 const inputPlayer1Name = document.getElementById("player1Name");
     inputPlayer1Name.addEventListener("change", function(){
         player1.name = inputPlayer1Name.value;
-        console.log(player1.name);
-    })
+    });
 
-    const inputPlayer2Name = document.getElementById("player2Name");
+const inputPlayer2Name = document.getElementById("player2Name");
     inputPlayer2Name.addEventListener("change", function(){
         player2.name = inputPlayer2Name.value;
-        console.log(player2.name);
-    })
+    });
+
+const checkbox = document.getElementById("robot");
+checkbox.addEventListener("change", function(){
+        gameControl.updateRobotStatus();
+});
